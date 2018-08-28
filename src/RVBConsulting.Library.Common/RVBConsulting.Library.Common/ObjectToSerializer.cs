a5ca5b3b2629bfaf6bafbf1c1e1bca7138b2d161
Rodrigo Vieira Baia <rodrigo@novasistemas.com.br>
2018-08-28 01:02:24 -0300

    Object Serializer

---
+++ src/RVBConsulting.Library.Common/RVBConsulting.Library.Common/ObjectToSerializer.cs
@@ -0,0 +1,373 @@
+﻿using System.IO;
+using System.IO.IsolatedStorage;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace RVBConsulting.Library.Common
+{
+    /// <summary>
+    /// Serialization format types.
+    /// </summary>
+    public enum SerializedFormat
+    {
+        /// <summary>
+        /// Binary serialization format.
+        /// </summary>
+        Binary,
+
+        /// <summary>
+        /// Document serialization format.
+        /// </summary>
+        Document
+    }
+
+    /// <summary>
+    /// Facade to XML serialization and deserialization of strongly typed objects to/from an XML file.
+    /// 
+    /// References: XML Serialization at http://samples.gotdotnet.com/:
+    /// http://samples.gotdotnet.com/QuickStart/howto/default.aspx?url=/quickstart/howto/doc/xmlserialization/rwobjfromxml.aspx
+    /// </summary>
+    public static partial class ObjectXMLSerializer<TEntity> where TEntity : class // Specify that T must be a class.
+    {
+        #region Load methods
+
+        /// <summary>
+        /// Loads an object from an XML file in Document format.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// serializableObject = ObjectXMLSerializer&lt;SerializableObject&gt;.Load(@"C:\XMLObjects.xml");
+        /// </code>
+        /// </example>
+        /// <param name="path">Path of the file to load the object from.</param>
+        /// <returns>Object loaded from an XML file in Document format.</returns>
+        public static TEntity Load(string path)
+        {
+            var serializableObject = LoadFromDocumentFormat(null, path, null);
+            return serializableObject;
+        }
+
+        /// <summary>
+        /// Loads an object from an XML file using a specified serialized format.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// serializableObject = ObjectXMLSerializer&lt;SerializableObject&gt;.Load(@"C:\XMLObjects.xml", SerializedFormat.Binary);
+        /// </code>
+        /// </example>		
+        /// <param name="path">Path of the file to load the object from.</param>
+        /// <param name="serializedFormat">XML serialized format used to load the object.</param>
+        /// <returns>Object loaded from an XML file using the specified serialized format.</returns>
+        public static TEntity Load(string path, SerializedFormat serializedFormat)
+        {
+            TEntity serializableObject = null;
+
+            switch (serializedFormat)
+            {
+                case SerializedFormat.Binary:
+                    serializableObject = LoadFromBinaryFormat(path, null);
+                    break;
+
+                case SerializedFormat.Document:
+                default:
+                    serializableObject = LoadFromDocumentFormat(null, path, null);
+                    break;
+            }
+
+            return serializableObject;
+        }
+
+        /// <summary>
+        /// Loads an object from an XML file in Document format, supplying extra data types to enable deserialization of custom types within the object.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// serializableObject = ObjectXMLSerializer&lt;SerializableObject&gt;.Load(@"C:\XMLObjects.xml", new Type[] { typeof(MyCustomType) });
+        /// </code>
+        /// </example>
+        /// <param name="path">Path of the file to load the object from.</param>
+        /// <param name="extraTypes">Extra data types to enable deserialization of custom types within the object.</param>
+        /// <returns>Object loaded from an XML file in Document format.</returns>
+        public static TEntity Load(string path, System.Type[] extraTypes)
+        {
+            TEntity serializableObject = LoadFromDocumentFormat(extraTypes, path, null);
+            return serializableObject;
+        }
+
+        /// <summary>
+        /// Loads an object from an XML file in Document format, located in a specified isolated storage area.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// serializableObject = ObjectXMLSerializer&lt;SerializableObject&gt;.Load("XMLObjects.xml", IsolatedStorageFile.GetUserStoreForAssembly());
+        /// </code>
+        /// </example>
+        /// <param name="fileName">Name of the file in the isolated storage area to load the object from.</param>
+        /// <param name="isolatedStorageDirectory">Isolated storage area directory containing the XML file to load the object from.</param>
+        /// <returns>Object loaded from an XML file in Document format located in a specified isolated storage area.</returns>
+        public static TEntity Load(string fileName, IsolatedStorageFile isolatedStorageDirectory)
+        {
+            TEntity serializableObject = LoadFromDocumentFormat(null, fileName, isolatedStorageDirectory);
+            return serializableObject;
+        }
+
+        /// <summary>
+        /// Loads an object from an XML file located in a specified isolated storage area, using a specified serialized format.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// serializableObject = ObjectXMLSerializer&lt;SerializableObject&gt;.Load("XMLObjects.xml", IsolatedStorageFile.GetUserStoreForAssembly(), SerializedFormat.Binary);
+        /// </code>
+        /// </example>		
+        /// <param name="fileName">Name of the file in the isolated storage area to load the object from.</param>
+        /// <param name="isolatedStorageDirectory">Isolated storage area directory containing the XML file to load the object from.</param>
+        /// <param name="serializedFormat">XML serialized format used to load the object.</param>        
+        /// <returns>Object loaded from an XML file located in a specified isolated storage area, using a specified serialized format.</returns>
+        public static TEntity Load(string fileName, IsolatedStorageFile isolatedStorageDirectory, SerializedFormat serializedFormat)
+        {
+            TEntity serializableObject = null;
+
+            switch (serializedFormat)
+            {
+                case SerializedFormat.Binary:
+                    serializableObject = LoadFromBinaryFormat(fileName, isolatedStorageDirectory);
+                    break;
+
+                case SerializedFormat.Document:
+                default:
+                    serializableObject = LoadFromDocumentFormat(null, fileName, isolatedStorageDirectory);
+                    break;
+            }
+
+            return serializableObject;
+        }
+
+        /// <summary>
+        /// Loads an object from an XML file in Document format, located in a specified isolated storage area, and supplying extra data types to enable deserialization of custom types within the object.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// serializableObject = ObjectXMLSerializer&lt;SerializableObject&gt;.Load("XMLObjects.xml", IsolatedStorageFile.GetUserStoreForAssembly(), new Type[] { typeof(MyCustomType) });
+        /// </code>
+        /// </example>		
+        /// <param name="fileName">Name of the file in the isolated storage area to load the object from.</param>
+        /// <param name="isolatedStorageDirectory">Isolated storage area directory containing the XML file to load the object from.</param>
+        /// <param name="extraTypes">Extra data types to enable deserialization of custom types within the object.</param>
+        /// <returns>Object loaded from an XML file located in a specified isolated storage area, using a specified serialized format.</returns>
+        public static TEntity Load(string fileName, IsolatedStorageFile isolatedStorageDirectory, System.Type[] extraTypes)
+        {
+            TEntity serializableObject = LoadFromDocumentFormat(null, fileName, isolatedStorageDirectory);
+            return serializableObject;
+        }
+
+        #endregion
+
+        #region Save methods
+
+        /// <summary>
+        /// Saves an object to an XML file in Document format.
+        /// </summary>
+        /// <example>
+        /// <code>        
+        /// SerializableObject serializableObject = new SerializableObject();
+        /// 
+        /// ObjectXMLSerializer&lt;SerializableObject&gt;.Save(serializableObject, @"C:\XMLObjects.xml");
+        /// </code>
+        /// </example>
+        /// <param name="serializableObject">Serializable object to be saved to file.</param>
+        /// <param name="path">Path of the file to save the object to.</param>
+        public static void Save(TEntity serializableObject, string path)
+        {
+            SaveToDocumentFormat(serializableObject, null, path, null);
+        }
+
+        /// <summary>
+        /// Saves an object to an XML file using a specified serialized format.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// SerializableObject serializableObject = new SerializableObject();
+        /// 
+        /// ObjectXMLSerializer&lt;SerializableObject&gt;.Save(serializableObject, @"C:\XMLObjects.xml", SerializedFormat.Binary);
+        /// </code>
+        /// </example>
+        /// <param name="serializableObject">Serializable object to be saved to file.</param>
+        /// <param name="path">Path of the file to save the object to.</param>
+        /// <param name="serializedFormat">XML serialized format used to save the object.</param>
+        public static void Save(TEntity serializableObject, string path, SerializedFormat serializedFormat)
+        {
+            switch (serializedFormat)
+            {
+                case SerializedFormat.Binary:
+                    SaveToBinaryFormat(serializableObject, path, null);
+                    break;
+
+                case SerializedFormat.Document:
+                    SaveToDocumentFormat(serializableObject, null, path, null);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Saves an object to an XML file in Document format, supplying extra data types to enable serialization of custom types within the object.
+        /// </summary>
+        /// <example>
+        /// <code>        
+        /// SerializableObject serializableObject = new SerializableObject();
+        /// 
+        /// ObjectXMLSerializer&lt;SerializableObject&gt;.Save(serializableObject, @"C:\XMLObjects.xml", new Type[] { typeof(MyCustomType) });
+        /// </code>
+        /// </example>
+        /// <param name="serializableObject">Serializable object to be saved to file.</param>
+        /// <param name="path">Path of the file to save the object to.</param>
+        /// <param name="extraTypes">Extra data types to enable serialization of custom types within the object.</param>
+        public static void Save(TEntity serializableObject, string path, System.Type[] extraTypes)
+        {
+            SaveToDocumentFormat(serializableObject, extraTypes, path, null);
+        }
+
+        /// <summary>
+        /// Saves an object to an XML file in Document format, located in a specified isolated storage area.
+        /// </summary>
+        /// <example>
+        /// <code>        
+        /// SerializableObject serializableObject = new SerializableObject();
+        /// 
+        /// ObjectXMLSerializer&lt;SerializableObject&gt;.Save(serializableObject, "XMLObjects.xml", IsolatedStorageFile.GetUserStoreForAssembly());
+        /// </code>
+        /// </example>
+        /// <param name="serializableObject">Serializable object to be saved to file.</param>
+        /// <param name="fileName">Name of the file in the isolated storage area to save the object to.</param>
+        /// <param name="isolatedStorageDirectory">Isolated storage area directory containing the XML file to save the object to.</param>
+        public static void Save(TEntity serializableObject, string fileName, IsolatedStorageFile isolatedStorageDirectory)
+        {
+            SaveToDocumentFormat(serializableObject, null, fileName, isolatedStorageDirectory);
+        }
+
+        /// <summary>
+        /// Saves an object to an XML file located in a specified isolated storage area, using a specified serialized format.
+        /// </summary>
+        /// <example>
+        /// <code>        
+        /// SerializableObject serializableObject = new SerializableObject();
+        /// 
+        /// ObjectXMLSerializer&lt;SerializableObject&gt;.Save(serializableObject, "XMLObjects.xml", IsolatedStorageFile.GetUserStoreForAssembly(), SerializedFormat.Binary);
+        /// </code>
+        /// </example>
+        /// <param name="serializableObject">Serializable object to be saved to file.</param>
+        /// <param name="fileName">Name of the file in the isolated storage area to save the object to.</param>
+        /// <param name="isolatedStorageDirectory">Isolated storage area directory containing the XML file to save the object to.</param>
+        /// <param name="serializedFormat">XML serialized format used to save the object.</param>        
+        public static void Save(TEntity serializableObject, string fileName, IsolatedStorageFile isolatedStorageDirectory, SerializedFormat serializedFormat)
+        {
+            switch (serializedFormat)
+            {
+                case SerializedFormat.Binary:
+                    SaveToBinaryFormat(serializableObject, fileName, isolatedStorageDirectory);
+                    break;
+
+                case SerializedFormat.Document:
+                default:
+                    SaveToDocumentFormat(serializableObject, null, fileName, isolatedStorageDirectory);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Saves an object to an XML file in Document format, located in a specified isolated storage area, and supplying extra data types to enable serialization of custom types within the object.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// SerializableObject serializableObject = new SerializableObject();
+        /// 
+        /// ObjectXMLSerializer&lt;SerializableObject&gt;.Save(serializableObject, "XMLObjects.xml", IsolatedStorageFile.GetUserStoreForAssembly(), new Type[] { typeof(MyCustomType) });
+        /// </code>
+        /// </example>		
+        /// <param name="serializableObject">Serializable object to be saved to file.</param>
+        /// <param name="fileName">Name of the file in the isolated storage area to save the object to.</param>
+        /// <param name="isolatedStorageDirectory">Isolated storage area directory containing the XML file to save the object to.</param>
+        /// <param name="extraTypes">Extra data types to enable serialization of custom types within the object.</param>
+        public static void Save(TEntity serializableObject, string fileName, IsolatedStorageFile isolatedStorageDirectory, System.Type[] extraTypes)
+        {
+            SaveToDocumentFormat(serializableObject, null, fileName, isolatedStorageDirectory);
+        }
+
+        #endregion
+
+        #region Private
+
+        private static FileStream CreateFileStream(IsolatedStorageFile isolatedStorageFolder, string path)
+        {
+            return isolatedStorageFolder == null ? new FileStream(path, FileMode.OpenOrCreate) : new IsolatedStorageFileStream(path, FileMode.OpenOrCreate, isolatedStorageFolder);
+        }
+
+        private static TEntity LoadFromBinaryFormat(string path, IsolatedStorageFile isolatedStorageFolder)
+        {
+            TEntity serializableObject = null;
+
+            using (var fileStream = CreateFileStream(isolatedStorageFolder, path))
+            {
+                var binaryFormatter = new BinaryFormatter();
+                serializableObject = binaryFormatter.Deserialize(fileStream) as TEntity;
+            }
+
+            return serializableObject;
+        }
+
+        private static TEntity LoadFromDocumentFormat(System.Type[] extraTypes, string path, IsolatedStorageFile isolatedStorageFolder)
+        {
+            TEntity serializableObject = null;
+
+            using (var textReader = CreateTextReader(isolatedStorageFolder, path))
+            {
+                var xmlSerializer = CreateXmlSerializer(extraTypes);
+                serializableObject = xmlSerializer.Deserialize(textReader) as TEntity;
+
+            }
+
+            return serializableObject;
+        }
+
+        private static TextReader CreateTextReader(IsolatedStorageFile isolatedStorageFolder, string path)
+        {
+            return isolatedStorageFolder == null ? new StreamReader(path) : new StreamReader(new IsolatedStorageFileStream(path, FileMode.Open, isolatedStorageFolder));
+        }
+
+        private static TextWriter CreateTextWriter(IsolatedStorageFile isolatedStorageFolder, string path)
+        {
+            return isolatedStorageFolder == null ? new StreamWriter(path) : new StreamWriter(new IsolatedStorageFileStream(path, FileMode.OpenOrCreate, isolatedStorageFolder));
+        }
+
+        private static XmlSerializer CreateXmlSerializer(System.Type[] extraTypes)
+        {
+            var ObjectType = typeof(TEntity);
+
+            XmlSerializer xmlSerializer = null;
+
+            xmlSerializer = extraTypes != null ? new XmlSerializer(ObjectType, extraTypes) : new XmlSerializer(ObjectType);
+
+            return xmlSerializer;
+        }
+
+        private static void SaveToDocumentFormat(TEntity serializableObject, System.Type[] extraTypes, string path, IsolatedStorageFile isolatedStorageFolder)
+        {
+            using (var textWriter = CreateTextWriter(isolatedStorageFolder, path))
+            {
+                var xmlSerializer = CreateXmlSerializer(extraTypes);
+                xmlSerializer.Serialize(textWriter, serializableObject);
+            }
+        }
+
+        private static void SaveToBinaryFormat(TEntity serializableObject, string path, IsolatedStorageFile isolatedStorageFolder)
+        {
+            using (var fileStream = CreateFileStream(isolatedStorageFolder, path))
+            {
+                var binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(fileStream, serializableObject);
+            }
+        }
+
+        #endregion
+    }
+
+}
---
+++ src/RVBConsulting.Library.Common/RVBConsulting.Library.Common/ObjectXMLSerializer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Xml.Serialization;
+
+namespace RVBConsulting.Library.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public static partial class ObjectXMLSerializer<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Generic file write routine in binary format
+        /// </summary>
+        /// <param name="fileName">File name</param>
+        /// <param name="obj">Object to insert into file</param>
+        public static void SaveToBinFile(string fileName, TEntity obj)
+        {
+            try
+            {
+                var streamWriter = new StreamWriter(fileName);
+                var binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(streamWriter.BaseStream, obj);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="obj"></param>
+        public static void SaveToStrFile(string fileName, TEntity obj)
+        {
+            try
+            {
+                var fileStream = new FileStream(fileName, FileMode.OpenOrCreate);
+                var soapFormatter = new SoapFormatter();
+                soapFormatter.Serialize(fileStream, obj);
+
+                fileStream.Close();
+                fileStream.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// Generic binary file read routine
+        /// </summary>
+        /// <param name="fileName">Name of the file to be recovered</param>
+        /// <returns>Object</returns>
+        public static TEntity LoadFromBinFile(string fileName)
+        {
+            try
+            {
+                var streamReader = new StreamReader(fileName);
+                var binaryFormatter = new BinaryFormatter();
+                var tipo = (TEntity)binaryFormatter.Deserialize(streamReader.BaseStream);
+                streamReader.Close();
+                return tipo;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="xml"></param>
+        /// <returns></returns>
+        public static TEntity Deserializar(string xml)
+        {
+            try
+            {
+
+                var reader = new StringReader(xml);
+                var serializer = new XmlSerializer(typeof(TEntity));
+
+                var tipo = (TEntity)serializer.Deserialize(reader);
+
+                return tipo;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Serializes an object
+        /// </summary>
+        /// <param name="obj">object</param>
+        /// <returns>string formato XML</returns>
+        public static string Serializar(TEntity obj)
+        {
+            var results = string.Empty;
+
+            var writer = new StringWriter();
+
+            var serializer = new XmlSerializer(obj.GetType());
+
+            serializer.Serialize(writer, obj);
+
+            results = writer.ToString();
+
+            return results.Replace("utf-16", "utf-8"); //donotlocalize
+        }
+    }
+}
